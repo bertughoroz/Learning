89a7e941235b47c04e5ca98e15ec4ad0935f00eb
Bertug <99143306+ber2h@users.noreply.github.com>
2022-10-21 19:57:43 +0300

    Add files via upload

---
+++ IfControl.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// if
+// And &&
+// or ||
+
+var myAge = 36
+
+if myAge < 30
+{
+    print("Sen 30 yasindan kücüksün")
+}
+else if myAge > 30 && myAge < 35
+{
+  print("Sen 30 ila 35 yasi arasindasin")
+}
+else
+{
+    print("Ya sok yaslisin ya cok genc")
+}
